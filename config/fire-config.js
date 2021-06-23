b5d93c021819fccb542b143d6e90e595607efaba
blake-hunsicker <blakeyhunsicker@gmail.com>
2021-06-23 11:38:43 -0400

    Test exposing projectId in fire-config

--- config/fire-config.js
+++ config/fire-config.js
@@ -3,7 +3,7 @@ import firebase from 'firebase'
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FB_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FB_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FB_PROJECT_ID,
+  projectId: "oracle-5bdbb",
   storageBucket: process.env.NEXT_PUBLIC_FB_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FB_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_APP_ID,
