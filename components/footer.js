98d4d1dd2c6f15d58d239a26a3273d545311a8bc
blake-hunsicker <blakeyhunsicker@gmail.com>
2021-06-25 18:07:35 -0400

    First pass on the front end

--- components/footer.js
+++ components/footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer>
-      2021 Blake Hunsicker
+      2021 <a href='https://twitter.com/blakehunsicker' target="_blank">Blake Hunsicker</a>
     </footer>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import fire from '../config/fire-config'
 import ReactMarkdown from 'react-markdown'
 import moment from 'moment'
@@ -6,48 +6,69 @@ import moment from 'moment'
 
 const Home = () => {
   const [summaries, setSummaries] = useState([])
-
+  const [articles, setArticles] = useState([])
+  const yesterday = moment().subtract(1, 'days').format('MM-D-y')
+  const today = moment().format('MM-D-y')
+  
   useEffect(() => {
-    const summaries = fire.firestore().collection('trends')
+    
+    const trendQuery = fire.firestore().collection('trends').where(fire.firestore.FieldPath.documentId(), '>=', today)
+    const yesterdayQuery = fire.firestore().collection('trends').where(fire.firestore.FieldPath.documentId(), '>=', yesterday).where(fire.firestore.FieldPath.documentId(), '<=', today)
+    const articleQuery = fire.firestore().collection('articles')
 
-    summaries.get().then((documentSnapshots) => {
+    trendQuery.get().then((documentSnapshots) => {
       const summaries = documentSnapshots.docs.map(doc => ({
         id: doc.id,
-        trend: doc.data().trend,
-        summary: doc.data().summary.replaceAll('- ', '\n - ').replaceAll('* ', '\n - '),
-        articles: doc.ref.collection('articles').onSnapshot((querySnapshot) => {
-          querySnapshot.forEach((doc) => {
-            hed: doc.data().headline
-          })
-        }),
+        ...doc.data()
       }))
       setSummaries(summaries)
     })
+    articleQuery.get().then((documentSnapshots) => {
+      const articles = documentSnapshots.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data()
+      }))
+      setArticles(articles)
+    })
+
   }, [])
 
-  console.log(summaries)
+  function toggleContext(e) {
+    e.preventDefault();
+    const context = e.currentTarget.nextElementSibling
+    context.classList.toggle('hidden')
+    e.currentTarget.classList.add('hidden')
+  }
 
   return (
     <>
       <section className='hero'>
         <h1>Diagram News</h1>
-        <h4>Minimal news summaries written by journalists and AI.</h4>
-      </section>    
-        {summaries.map(summary =>
-          <>
-            <section className='summary'>
-              <h3>{summary.trend}</h3>
-              <ReactMarkdown>{summary.summary}</ReactMarkdown>
-              <button>Go deeper</button>
-            </section>
-            <div className='context-card'>
-              {/* {summary.articles.map(article =>
-                <p>{article.headline}</p>
-              )} */}
-            </div>
-          </> 
-        )}
-      <button className='pagination'>Read Yesterday's Stories</button>
+        <h4>Minimal news summaries written by journalists and <a href='https://beta.openai.com/'>GPT-3</a></h4>
+      </section>
+      {summaries.map(summary =>
+        <section className='summary'>
+          <h4 className='headline'>{summary.trend}</h4>
+          <ReactMarkdown>{summary.summary.replaceAll('- ', '\n - ').replaceAll('* ', '\n - ')}</ReactMarkdown>
+          <button onClick={toggleContext}>View sources</button>
+          <div className='context hidden'>
+            <p>Sources</p>
+            <ul>
+              {articles.map(article =>
+              article.trend != summary.trend ?
+                (null)
+              :
+                (
+                  <li className='source'>
+                    <a href={article.url} target='_blank'>{article.headline.replaceAll('<b>','').replaceAll('</b>','').replaceAll('&#39;',"'")}</a>
+                  </li>          
+                )
+              )}
+            </ul>
+          </div>
+        </section>
+      )}
+      {/* <button className='pagination'>Read Yesterday's Stories</button> */}
     </>
   )
 }
